30f05d16105bb8a37d307deeecd2512d349149a3
Brandeborg <Hallager2505@gmail.com>
2023-08-19 17:19:01 +0200

    Added another test command

--- src/discord_coc.py
+++ src/discord_coc.py
@@ -4,13 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_BOT_API_TOKEN")
 
-import discord
 from discord.ext import commands
-
 bot = commands.Bot()
 
 @bot.slash_command(name="repeat", description="Repeats the argument", guild_ids=[1142445213084815450])
 async def repeat(ctx, arg):
     await ctx.respond(arg)
+
+@bot.slash_command(name="add", description="Adds two numbers", guild_ids=[1142445213084815450])
+async def repeat(ctx, num1, num2):
+    await ctx.respond(int(num1) + int(num2))
 
 bot.run(DISCORD_TOKEN)
