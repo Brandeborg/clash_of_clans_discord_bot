eae7dba5267b52194db809891e86612c49024cbe
Brandeborg <hallager2505@gmail.com>
2023-09-04 18:16:30 +0200

    removed initial test commads

--- src/discord_coc.py
+++ src/discord_coc.py
@@ -24,33 +24,6 @@ bot = commands.Bot()
 coc = clash_of_clans.CoCAPI()
 
 # commands
-@bot.slash_command(name="player_name", description="Returns the player's Clash of Clans name", guild_ids=[DISCORD_SERVER_ID])
-async def coc_playername(ctx, playertag: Option(str, "Enter your CoC player tag", required=False, default=None)):
-    """Sends a response containing a Clash of Clans player name, 
-    either by looking up an explicitly passed playertag or by extracting a playertag from
-    the discord user's display name.
-
-    Args:
-        ctx (_type_): Discord context, containing attributes such as displayname and functions
-        playertag (Option, optional): A CoC player tag. Defaults to False, default=None).
-
-    Returns:
-        None: Returns nothing
-    """
-    # fetch data
-    try:
-        playertag = await bot_util.get_playertag(ctx.author.display_name) if not playertag else playertag
-        playertag = bot_util.add_octothorpe(playertag)
-        bot_util.validate_tag(playertag)
-        player = coc.player(playertag)
-    except Exception as e:
-        return await ctx.respond(e)
-    
-    # format response
-
-    # send response
-    await ctx.respond(player["name"])
-
 @bot.slash_command(name="player_progress", description="Returns the players progress towards maxing current TH", guild_ids=[DISCORD_SERVER_ID])
 async def coc_player_progress(ctx, playertag: Option(str, "Enter a CoC player tag", required=False, default=None)):
     """Sends a response containing a Clash of Clans player's progress of upgrading:
@@ -272,34 +245,6 @@ async def coc_player_progress_spells(ctx, playertag: Option(str, "Enter a CoC pl
     # send response
     await ctx.respond(title, file=discord.File(plt_file_path))
     os.remove(plt_file_path)
-
-@bot.slash_command(name="clan_name", description="Returns the player's Clash of Clans clan name", guild_ids=[DISCORD_SERVER_ID])
-async def coc_clanname(ctx, playertag: Option(str, "Enter your CoC player tag", required=False, default=None),
-                       clantag: Option(str, "Enter your CoC clan tag", required=False, default=None)):
-    """Sends a response containing a Clash of Clans clan name, 
-    either by looking up an explicitly passed playertag or clantag or by extracting a playertag from
-    the discord user's displayname. If no clan tag is passed, the player tag will be used to fetch a 
-    clantag associated with the player.
-
-    Args:
-        ctx (_type_): Discord context, containing attributes such as displayname and functions
-        playertag (Option, optional): A CoC player tag. Defaults to False, default=None).
-        clantag (Option, optional): A CoC clan tag. Defaults to False, default=None).
-
-    Returns:
-        _type_: _description_
-    """
-    # fetch data
-    try:
-        clantag: str = await bot_util.handle_clantag_options(ctx.author.display_name, playertag, clantag)
-        clan = coc.clan(clantag)
-    except Exception as e:
-        return await ctx.respond(e)
-
-    # format response
-
-    # send response
-    await ctx.respond(clan["name"])
 
 @bot.slash_command(name="current_war", description="Returns details about a clan's ongoing war", guild_ids=[DISCORD_SERVER_ID])
 async def current_war(ctx, playertag: Option(str, "Enter your CoC player tag", required=False, default=None),
